52ab363e2b8a05fd778c6c7e1ee6808c7d1dd7ce
Miguel Macias <migxmac@users.noreply.github.com>
2016-09-04 16:54:58 -0500

    change x -> z and 4 to 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,9 @@
-def my_square(x):
+def my_square(z):
 	"""takes a value and returns the square value.
 	
 	uses the ** operator
 	"""
-	return(x ** 2)
+	return(z * 2)
 
-print(my_square(4))
+print(my_square(24))
 
